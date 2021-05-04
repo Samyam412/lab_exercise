46c49a0c708de2ab5e09443c5b3af01106b6bd43
Samyam412 <Kcsamy50@gmail.com>
2021-05-04 10:29:55 +0545

    ans

--- lab exercise 1/question 4.py
+++ lab exercise 1/question 4.py
@@ -4,3 +4,10 @@ The program should print two numbers: the number of hours (between 0 and 23) and
 number of minutes (between 0 and 59).
 For example, if N = 150, then 150 minutes have passed since midnight - i.e. now is 2:30
 am. So, the program should print 2 30"""
+
+time_passed = int(input("enter the time passed since midnight in minutes:"))
+
+hour = time_passed // 60
+minutes = time_passed % 60
+
+print(f"Currently it's {hour}:{minutes}am")
