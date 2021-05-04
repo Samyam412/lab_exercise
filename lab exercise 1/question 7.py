65d724e4a0a2c6ce136cedcd2431f5614d7df437
Samyam412 <Kcsamy50@gmail.com>
2021-05-04 12:01:59 +0545

    ans

--- lab exercise 1/question 7.py
+++ lab exercise 1/question 7.py
@@ -5,5 +5,10 @@ jogging the last at 7mph again. Will this be quicker or slower than the bus?"""
 
 
 distance = 4
-time_taken = 0
+time_taken_bus = (distance / 25) + 10*2
+time_taken_man =distance / (7*2 + 15*2)
 
+if time_taken_man > time_taken_bus:
+    print("you can jog to the university faster")
+else:
+    print("you can reach the university faster by the bus")
